b45d15db372a2c699251be512974b0d7051e6775
kartiks2000 <46376692+kartiks2000@users.noreply.github.com>
2020-08-07 22:41:03 +0530

    Add files via upload

---
+++ phpdemo2/session1.php
@@ -0,0 +1,7 @@
+<?php session_start();
+
+    $_SESSION['greet'] = "hello Bro!!!";
+
+    echo "Go to /session1  to see the session data....";
+
+?>
---
+++ phpdemo2/session2.php
@@ -0,0 +1,12 @@
+<?php session_start();
+
+    // echo $_SESSION['greet'];
+    
+
+    // OR....
+
+    $q =  $_SESSION['greet'];
+
+    echo "<h1>$q</h1>" ;
+
+?>
